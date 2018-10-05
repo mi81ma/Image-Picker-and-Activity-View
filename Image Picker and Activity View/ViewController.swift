3955f66f3518b15663c961110611b1f506f57dea
masato <masato@m-mbp.local>
2018-10-05 13:09:01 +0800

    test not run

--- Image Picker and Activity View/ViewController.swift
+++ Image Picker and Activity View/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var button: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // button instance
+//        let button = UIButton()
+        view.addSubview(button)
+
+    }
+
+
+    // ******************************************
+    // overRide the "button" Layout
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        // Layout
+        // set BackGround Color
+        button.backgroundColor = UIColor.blue
+
+        // set Posision
+        button.frame = CGRect(x: view.frame.origin.x + 50,
+                              y: view.safeAreaInsets.top + 300,
+                              width: view.frame.width - 80 * 2,
+                              height: 100)
     }
 
 
